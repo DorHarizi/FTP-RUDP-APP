1335d6c02e594dbcab5a1b2c3add028d3cc5490d
DorHarizi <93187894+DorHarizi@users.noreply.github.com>
2023-03-13 23:30:22 +0200

    Update client_dhcp.py

--- client_dhcp.py
+++ client_dhcp.py
@@ -7,37 +7,44 @@
 
     #   def __init__(self, port_dhcp):
     #
-    #     self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    #     self.ip = "0.0.0.0"
-    #     self.port = port_dhcp
-    #     self.ip_table = {}
-    #     self.ip_rcv = ""
-    #     self.is_connected = True
-    #     self.run()
-    #
-    #
-    #
-    #
-    #  def send_message(self, message):
-    #
-    #      message = message.encode('utf-8')
-    #      print(f"massege sent:{message}")
-    #      self.socket.sendto(message, (self.ip, self.port))
-    #      while i < 2:
-    #         data = self.socket.recv(1024).decode('utf-8')
-    #             if data == "ACK":
-    #                  print(f"The IP of the client from the dhcp server is: {self.ip_rcv}")
-    #                  print("the connection is done")
-    #                  i=1
-    #             else:
-    #                 self.ip = data
-    #                 print(f"this is the chosen ip{self.ip}")
-    #                 i=2
-    #
-    #
-    # def disconnect(self):
-    #             print("the client disconnected from the server")
-    #             self.socket.close()
-    #
-    #
-    #
+import socket
+import threading
+
+FORMAT = 'utf-8'
+
+
+class client_dhcp:
+
+    def __init__(self):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.ip = socket.gethostbyname(socket.gethostname())
+        self.port = 68
+        self.ip_table = {}
+        self.ip_rcv = ""
+        self.is_connected = True
+
+    def send_message(self):
+        print("Discovery Message")
+        self.socket.sendto("discovery".encode(FORMAT), (self.ip, self.port))
+        print("message sent")
+
+    def get_ip(self):
+        self.socket.sendto("discovery".encode(FORMAT), (self.ip, self.port))
+        new_ip, addr = self.socket.recvfrom(1024)
+        new_ip = new_ip.decode(FORMAT)
+        print(f"the new ip is{new_ip}")
+        print("I want this IP")
+        self.socket.sendto("ACK".encode(FORMAT), (self.ip, self.port))
+        return new_ip
+
+    def run(self):
+        self.socket.bind((self.ip, self.port))
+        print("the client is connected to the server")
+
+    def disconnect(self):
+        print("the client disconnected from the server")
+        self.socket.close()
+
+
+if __name__ == "__main__":
+    client_dhcp().get_ip()

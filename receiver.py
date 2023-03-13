e1efacfa11b70e747bdb923c10dd2479dd3759f5
DorHarizi <93187894+DorHarizi@users.noreply.github.com>
2023-03-13 23:34:40 +0200

    Update receiver.py

--- receiver.py
+++ receiver.py
@@ -1,63 +1,37 @@
-# import socket
-import rudp_socket
-from rudp_socket import RUDPSocket, RUDPPacket
+import socket
 
 
 def receive_file(filename, ip, port):
 
-    # rudp_socket_inst = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    # rudp_socket_inst.bind((ip, port))
-    rudp_socket_inst = RUDPSocket()
-    rudp_socket_inst.socket.bind(ip, port)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind((ip, port))
     print(f"{ip}, {port}")
-    # list_packet = {}
 
     with open(filename, "wb") as f:
-        print("write")
         expectedseqnum = 0
         while True:
-            try:
-                data , addr= rudp_socket_inst.socket.recvfrom(1024)
-                packet = RUDPPacket.unpack(data)
-                if packet.seqnum == -1:
-                    break
-                # print(data)
-                ack_packet = RUDPPacket()
-                ack_packet.seqnum = packet.seqnum
-                if packet.seqnum == expectedseqnum:
-                    f.write(packet.data)
-                    f.flush()
-                    print("write to the file")
-                    expectedseqnum += 1
-                    print(f"send ACK{packet.seqnum}")
-                    rudp_socket_inst.sendto(data, addr)
-                elif packet.seqnum < expectedseqnum:
-                    print(f"send ACK{seqnum}")
-                    rudp_socket_inst.sendto(data, addr)
-            except Exception as e:
-                print(e)
+            data, addr = sock.recvfrom(1024)
+            seqnum = int.from_bytes(data[:4], byteorder="big", signed=True)
+            print(f"the seqnum is: {seqnum}")
+            packet = data[4:]
+            if seqnum == -1:
+                break
+            # print(data)
+            if seqnum == expectedseqnum:
+                f.write(packet)
+                f.flush()
+                expectedseqnum += 1
+                print(f"send ACK: {seqnum}")
+                sock.sendto(data[:4], addr)
+            elif seqnum < expectedseqnum:
+                print(f"send ACK: {seqnum}")
+                sock.sendto(data[:4], addr)
     print("close connection")
-    rudp_socket_inst.socket.close()
+    sock.close()
 
 
 if __name__ == "__main__":
-    receive_file("server_files/delete_big.png", "localhost", 1234)
-
-#
-# def checksum(data):
-#     checksum = 0
-#     for byte in data:
-#         checksum += byte
-#         if checksum > 0xFF:
-#             checksum &= 0xFF
-#             checksum += 1
-#     return checksum
-#
-#
-# def deliver_data(data):
-#     # return the process can continue
-#     return data
-#
+    receive_file("delete_big.png", "localhost", 1234)
 #
 # def extract(packet):
 #     # decode the data from the packet that the sender send

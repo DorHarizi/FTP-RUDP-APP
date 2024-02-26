adce2339f7a252a83433925c3e5f4d99b3f3af90
DorHarizi <93187894+DorHarizi@users.noreply.github.com>
2024-02-26 19:45:13 +0200

    Update client_dhcp.py

--- client_dhcp.py
+++ client_dhcp.py
@@ -1,50 +1,25 @@
-
-# import socket
-#
-#
-#
-# class client_dhcp:
-
-    #   def __init__(self, port_dhcp):
-    #
 import socket
 import threading
 
 FORMAT = 'utf-8'
 
-
 class client_dhcp:
+    """
+    A simplified DHCP client simulation.
+
+    This class implements the basic functionality of a DHCP client, capable of sending
+    a discovery message to a DHCP server, receiving an IP address offer, and sending
+    an acknowledgment for the received IP address.
+
+    Attributes:
+        socket (socket.socket): UDP socket for sending and receiving messages.
+        ip (str): IP address of the client, determined by the hostname.
+        port (int): The port number used by the client (default is 68, the DHCP client port).
+        ip_table (dict): A dictionary to store IP addresses (unused in this snippet).
+        ip_rcv (str): The received IP address from the server.
+        is_connected (bool): Flag to indicate if the client is connected.
+    """
 
     def __init__(self):
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.ip = socket.gethostbyname(socket.gethostname())
-        self.port = 68
-        self.ip_table = {}
-        self.ip_rcv = ""
-        self.is_connected = True
-
-    def send_message(self):
-        print("Discovery Message")
-        self.socket.sendto("discovery".encode(FORMAT), (self.ip, self.port))
-        print("message sent")
-
-    def get_ip(self):
-        self.socket.sendto("discovery".encode(FORMAT), (self.ip, self.port))
-        new_ip, addr = self.socket.recvfrom(1024)
-        new_ip = new_ip.decode(FORMAT)
-        print(f"the new ip is{new_ip}")
-        print("I want this IP")
-        self.socket.sendto("ACK".encode(FORMAT), (self.ip, self.port))
-        return new_ip
-
-    def run(self):
-        self.socket.bind((self.ip, self.port))
-        print("the client is connected to the server")
-
-    def disconnect(self):
-        print("the client disconnected from the server")
-        self.socket.close()
-
-
-if __name__ == "__main__":
-    client_dhcp().get_ip()
+        """
+        Initializes the client with a UDP socket and binds to the DHCP

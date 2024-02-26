c0dcbf6ef26060d6cc83229e23734f719b094960
DorHarizi <93187894+DorHarizi@users.noreply.github.com>
2024-02-26 20:29:43 +0200

    Update receiver.py

--- receiver.py
+++ receiver.py
@@ -1,55 +1,49 @@
 import socket
 
-
 def receive_file(filename, ip, port):
+    """
+    Receives a file over UDP, implementing basic reliability through sequence numbers.
 
+    This function sets up a UDP socket to receive packets of a file being sent to it. It ensures
+    that packets are processed in the correct order by checking sequence numbers, writing the data
+    to a file, and sending acknowledgments (ACKs) back to the sender for each packet received in order.
+
+    Parameters:
+    - filename: The name of the file to save the received data to.
+    - ip: The IP address to bind the receiving socket to.
+    - port: The port number on which to listen for incoming data.
+    """
+    # Create and bind a UDP socket to the specified IP and port.
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind((ip, port))
-    print(f"{ip}, {port}")
+    print(f"Listening on {ip}, port {port}")
 
     with open(filename, "wb") as f:
-        expectedseqnum = 0
+        expectedseqnum = 0  # Initialize the expected sequence number to 0
         while True:
-            data, addr = sock.recvfrom(1024)
-            seqnum = int.from_bytes(data[:4], byteorder="big", signed=True)
-            print(f"the seqnum is: {seqnum}")
-            packet = data[4:]
-            if seqnum == -1:
-                break
-            # print(data)
-            if seqnum == expectedseqnum:
-                f.write(packet)
-                f.flush()
-                expectedseqnum += 1
-                print(f"send ACK: {seqnum}")
-                sock.sendto(data[:4], addr)
-            elif seqnum < expectedseqnum:
-                print(f"send ACK: {seqnum}")
-                sock.sendto(data[:4], addr)
-    print("close connection")
-    sock.close()
+            data, addr = sock.recvfrom(1024)  # Receive data packet
+            seqnum = int.from_bytes(data[:4], byteorder="big", signed=True)  # Extract sequence number
+            print(f"Received packet with sequence number: {seqnum}")
+            packet = data[4:]  # Actual file data starts from byte 4
 
+            if seqnum == -1:
+                # A sequence number of -1 indicates the end of file transfer
+                break
+
+            if seqnum == expectedseqnum:
+                # If the packet is the one we're expecting, write to file and increment expected sequence number
+                f.write(packet)
+                f.flush()  # Ensure data is written to disk
+                expectedseqnum += 1
+                print(f"Sending ACK for sequence number: {seqnum}")
+                sock.sendto(data[:4], addr)  # Send ACK for the received packet
+            elif seqnum < expectedseqnum:
+                # If an earlier packet is received, resend the ACK (it may have been lost)
+                print(f"Resending ACK for sequence number: {seqnum}")
+                sock.sendto(data[:4], addr)
+
+    print("File transfer complete, connection closed.")
+    sock.close()  # Close the socket once file transfer is complete
 
 if __name__ == "__main__":
-    receive_file("delete_big.png", "localhost", 1234)
-#
-# def extract(packet):
-#     # decode the data from the packet that the sender send
-#     return packet[0].decode('utf-8')
-#
-#
-# def corrupt(data, check_sum):
-#     rcv_checksum = checksum(data)
-#     if rcv_checksum == check_sum:
-#         return True
-#     else:
-#         return False
-#
-#
-# def rdt_rcv(packet, address_source):
-#     # reliable data transfer receive from the sender
-#     data = exstract(packet)
-#     if not corrupt(data, packet[1]):
-#         deliver_data(data)
-#     else:
-#         pass
+    receive_file("path/to/your/file.png", "localhost", 1234)
